a08a7f8a5fbdf9413579809dd0c9c58762d620c1
Akhil Jayan <akhiljayan29.aj@gmail.com>
2020-04-23 15:06:59 +0530

    Using Console

---
+++ 1. Using Console/app.js
@@ -0,0 +1,30 @@
+// log to console
+
+console.log("hello");
+console.log(123);
+console.log(true);
+var greeting = 'Heyo';
+console.log(greeting);
+console.log([1,2,3]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning');
+console.time('Time');
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+console.timeEnd('Time');
