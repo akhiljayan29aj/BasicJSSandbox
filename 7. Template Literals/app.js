0dc767c2648f2b5531c8a2b2e005335d3cf5e981
Akhil Jayan <akhiljayan29.aj@gmail.com>
2020-04-26 13:31:18 +0530

    Template string/literal

---
+++ 7. Template Literals/app.js
@@ -0,0 +1,34 @@
+const name = 'akhil';
+const age = 20;
+const job = 'web dev';
+const city = 'delhi';
+let html;
+
+// without template strings (es5)
+html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>Job: ' + job + '</li><li>City: ' + city + '</li></ul>';
+
+html = '<ul>' +
+       '<li>Name: ' + name + '</li>' +
+       '<li>Age: ' + age + '</li>' +
+       '<li>Job: ' + job + '</li>' +
+       '<li>City: ' + city + '</li>' +
+       '</ul>';
+
+function hello(){
+  return 'hello';
+}
+
+// With template strings (es6)
+html = `
+  <ul style='list-style: none'>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2 + 2}</li>
+    <li>${hello()}</li>
+    <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+  </ul>
+`;
+
+document.body.innerHTML = html; 
